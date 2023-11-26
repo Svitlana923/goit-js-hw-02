6a36f711e3e3e55b13e36620a591d6f91dcf58f6
Svitlana923 <svitlanakostiuk11@gmail.com>
2023-11-27 00:43:49 +0100

    2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
 "use strict";
 function formatMessage(message, maxLength) { 
-    if(message >= maxLength) {
-        return message.slice(0, maxLength) + "...";
-    } else { return message };
+    if(message.length <= maxLength) {
+        return message; 
+    } else { return message.slice(0, maxLength) + "..."; };
 };
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
